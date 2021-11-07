cb4b0f98cc70434979736a81ac677d4fcede2725
kn201905 <zz2018-dr@yahoo.co.jp>
2021-11-07 21:17:36 +0900

    Update Program.cs

--- temp/001/Program.cs
+++ temp/001/Program.cs
@@ -15,18 +15,18 @@ namespace codeshare
 	
 	static class Server
 	{
-		static TcpListener ms_TcpListener_http;
+		static TcpListener ms_server_TcpListener;
 
 		public static async Task Start()
 		{
 			Console.WriteLine("--- サーバーを起動します ---");
 
-			ms_TcpListener_http = new TcpListener(System.Net.IPAddress.Parse("127.0.0.1"), 80);
-			ms_TcpListener_http.Start();
+			ms_server_TcpListener = new TcpListener(System.Net.IPAddress.Parse("127.0.0.1"), 80);
+			ms_server_TcpListener.Start();
 
 			for (;;)
 			{
-				TcpClient tcp_client = await ms_TcpListener_http.AcceptTcpClientAsync();
+				TcpClient tcp_client = await ms_server_TcpListener.AcceptTcpClientAsync();
 
 				Console.WriteLine("--- 接続を検知しました ---");
 			}
